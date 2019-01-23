e3cf235dac21eb7745294a7d0ea0d590c28b046b
MatejMaricic <matej.maricic@gmail.com>
2019-01-23 10:49:54 +0100

    added input form for row and col, ciklicnaMatrica function for determining position of each number in two dimenisonal array, var_dump array to test output

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+
+$row = $_POST['row']; //2
+$col= $_POST['col']; //4
+
+
+function ciklicnaMatrica($row, $col, &$array)
+{
+
+    $pos = 1;  //pozicija u matrici
+    $row_s = 0; // pocetni redak
+    $col_s = 0; // pocetni stupac
+
+    while ($row_s < $row && $col_s < $col) {
+
+        for ($i = $col_s; $i < $col; $i++)
+        {
+            $array[$row_s][$i] = $pos++;
+        }
+        $row_s++;
+
+
+        for ($i = $row_s; $i < $row; $i++)
+        {
+            $array[$i][$col - 1] = $pos++;
+        }
+        $col--;
+
+
+        if ($row_s < $row) {
+            for ($i = $col - 1; $i >= $col_s; $i--)
+            {
+                $array[$row - 1][$i] = $pos++;
+            }
+
+            $row--;
+        }
+
+
+        if ($col_s < $col) {
+            for ($i = $row - 1; $i >= $row_s; $i--)
+                $array[$i][$col_s] = $pos++;
+            $col_s++;
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,71 @@
+<?php require 'functions.php'; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Zadaca 3</title>
+</head>
+<body>
+
+
+<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" >
+   Broj redaka   <input type="text" name="row"><br>
+   Broj stupaca  <input type="text" name="col"><br>
+    <input type="submit" value="Pošalji" >
+</form>
+
+<?php
+
+$row = $_POST['row'];
+$col= $_POST['col'];
+
+
+ciklicnaMatrica($row, $col,$array);
+
+
+echo '<pre>';
+print_r($array);
+echo '<pre/>'
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
