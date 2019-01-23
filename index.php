5c98a1ed31ada7d5ede3681c4a60afc8a9ed2d49
MatejMaricic <matej.maricic@gmail.com>
2019-01-23 18:04:29 +0100

    added bootstrap and changed html and css for index

--- index.php
+++ index.php
@@ -8,53 +8,73 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Zadaca 3</title>
 </head>
 <body>
 
-
-<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" >
-   Broj redaka <br>
-    <input class="input-css" type="text" name="rows" value="<?php echo $_POST['rows'] ?>"><br>
-   Broj stupaca <br>
-    <input class="input-css" type="text" name="cols" value="<?php echo $_POST['cols'] ?>"><br>
-    <input type="submit"  value="KREIRAJ TABLICU" class="button-css" >
-</form>
-
 <?php
-
 $rows = filter_input(INPUT_POST, 'rows', FILTER_SANITIZE_NUMBER_INT);
 $cols = filter_input(INPUT_POST, 'cols', FILTER_SANITIZE_NUMBER_INT);
-if( !$rows || !$cols) { echo "Morate upisati cijeli broj"; }
-
-
-
-$numbers = ciklicnaMatrica($rows, $cols);
-
-
-
 ?>
-<table>
-    <tbody>
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">PHP Akademija - Zadatak 3</a>
+</nav>
+<div class="container space">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-sm">
 
-     <?php for ($i = 0; $i < $rows; $i++){ ?>
+                    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                        <div class="form-group" >
+                            <label for="brojRedaka">Broj Redaka</label> <br>
+                            <input type="number" class="input-css" id="brojRedaka" name="rows" value="<?php echo $rows; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="brojStupaca">Broj stupaca</label><br>
+                            <input type="number" class="input-css" id="brojStupaca" name="cols" value="<?php echo $cols; ?>">
+                        </div>
+                        <input type="submit" class="btn btn-success" value="KREIRAJ TABLICU" class="btn btn-primary">
+                    </form>
+                </div>
+                <div class="col-sm">
+                    <?php
+                    if( !$rows || !$cols) {
+                        echo "Morate upisati cijeli broj";
+                    }
+                    $numbers = ciklicnaMatrica($rows, $cols);
 
-    <tr>
+                    ?>
+                    <table>
+                        <tbody>
 
-     <?php for ($j = 0; $j < $cols; $j++){ ?>
+                        <?php for ($i = 0; $i < $rows; $i++){ ?>
 
-         <td>
-         <?php   echo($numbers[$i][$j]);  ?>
-        </td>
+                            <tr>
 
-        <?php } ?>
+                                <?php for ($j = 0; $j < $cols; $j++){ ?>
 
-    </tr>
+                                    <td>
+                                        <?php   echo($numbers[$i][$j]);  ?>
+                                    </td>
 
-   <?php } ?>
+                                <?php } ?>
 
-    </tbody>
-</table>
+                            </tr>
+
+                        <?php } ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 
 
 </body>
